a3ff9b7c09dc6a37eb7cfb8ff07ae3c772eeb5ce
osemenets21 <oleg.min.vin@gmail.com>
2023-05-23 18:38:01 +0200

    send all

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    const animatedElements = document.querySelectorAll('.header-logo');
+    animatedElements.forEach(function(element) {
+      element.classList.add('header-logo');
+    });
+  });
+  
